859eeed9443a6f6be947b0264d41df2adcaf7354
LDmitriyV <71625536+LDmitriyV@users.noreply.github.com>
2022-11-29 16:05:11 +0600

    Update JUnitTest.java

--- test/ldv9_JUnit/JUnitTest.java
+++ test/ldv9_JUnit/JUnitTest.java
@@ -7,17 +7,17 @@ import static javafxapplication.FXMLDocumentController.completing;
 
 public class JUnitTest {
         
-    @Test
+ @Test
     public void test1FXML() throws Exception {
-        assertTrue(completing(2, 2, 4) == 1.0);
-        assertTrue(completing(10, 10, 10) == 4000.00);
-        assertTrue("Проверка деления ", completing(3, 3, 5) == 0.5);
+        assertTrue(completing(2, 3, 6) == 36);
+        assertTrue(completing(5, 5, 5) == 1.20);
+        assertTrue("Проверка деления ", completing(3, 3, 5) == 0.72);
     }
     
     @Test
     public void test2FXML() throws Exception {
         try {
-            assertTrue("Проверка деления на 0", completing(0, 0, 4) == 0);
+            assertTrue("Проверка деления на 0", completing(0, 3, 0) == 0);
             fail("Нет ошибки при делении на 0");
         } catch (Exception thrown) {
             assertNotEquals("", thrown.getMessage());
